0caa945a41fb141f8ad138f23d74759ca468b12e
Brian-NN <brianndungu274@gmail.com>
2025-11-16 12:50:45 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,10 @@
 <?php
-// Database configuration (if using MySQL)
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'university_db');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
-// Site configuration
+
 define('SITE_NAME', 'XYZ University');
 define('SITE_EMAIL', 'info@xyzuniversity.ac.ke');
 define('ADMIN_EMAIL', 'admin@xyzuniversity.ac.ke');
